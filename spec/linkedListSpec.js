3dd122754a021fd01a57a34d20e220866810e8fd
Hack Reactor Students <github@hackreactor.com>
2013-06-20 17:14:40 -0700

    linkedlist done

--- spec/linkedListSpec.js
+++ spec/linkedListSpec.js
@@ -16,5 +16,18 @@ describe("linkedList", function() {
     expect(linkedList.contains).toEqual(jasmine.any(Function));
   });
 
+  it("should test if the linkedlist contains a value", function(){
+    linkedList.addToTail(5);
+    linkedList.addToTail(10);
+    linkedList.addToTail(14);
+    linkedList.addToTail(20);
+    expect(linkedList.contains(10)).toEqual(true);
+  });
+   it("should remove the head and return its value and assign head to head.next", function(){
+    linkedList.addToTail(5);
+    linkedList.addToTail(10);
+    expect(linkedList.removeHead()).toEqual(5);
+    expect(linkedList.head.value).toEqual(10);
+  });
   // add more tests here to test the functionality of linkedList
 });
--- src/linkedList.js
+++ src/linkedList.js
@@ -4,13 +4,36 @@ var makeLinkedList = function(){
   newLinkedList.head = null;
   newLinkedList.tail = null;
 
-  newLinkedList.addToTail = function(){
+  newLinkedList.addToTail = function(value){
+    if (newLinkedList.head === null){
+      newLinkedList.head = makeNode(value);
+      newLinkedList.tail =  newLinkedList.head;
+    } else {
+      newLinkedList.tail.next = makeNode(value);
+      newLinkedList.tail = newLinkedList.tail.next;
+    }
   };
 
   newLinkedList.removeHead = function(){
+    var temp = newLinkedList.head.value;
+    newLinkedList.head = newLinkedList.head.next;
+    return temp;
   };
 
-  newLinkedList.contains = function(){
+  newLinkedList.contains = function(value){
+    var current=newLinkedList.head;
+    while(current.next !== null){
+      if (current.value==value){
+        return true;
+      }
+      current=current.next;
+    }
+    return false;
+
+    // console.log(newLinkedList.head.value);
+    // console.log(newLinkedList.head.next);
+    // console.log(newLinkedList.head.next.next);
+    // console.log(newLinkedList.tail);
   };
 
   return newLinkedList;
@@ -21,8 +44,10 @@ var makeNode = function(value){
   newNode.value = value;
   newNode.next = null;
 
+
   newNode.removeNextNode = function(){
   };
 
   return newNode;
 };
+
