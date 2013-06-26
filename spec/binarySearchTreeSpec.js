4d5ea4731d659d911a531d34c5544cd092763dfa
Hack Reactor Students <github@hackreactor.com>
2013-06-25 22:38:01 -0700

    Finished binarySearchTree.

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -2,13 +2,40 @@ describe("binarySearchTree", function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = makeBinarySearchTree();
+    binarySearchTree = makeBinarySearchTree(6);
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(binarySearchTree.children).toEqual(jasmine.any(Function));
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
-  // add more tests here to test the functionality of binarySearchTree
+
+  it("insert < 6 will add a value to left and insert > 6 will add value right", function() {
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(8);
+    expect(binarySearchTree.left.length).toEqual(2);
+    expect(binarySearchTree.right.length).toEqual(2);
+  });
+
+  it("contains should check value and return true if found", function(){
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(9);
+    expect(binarySearchTree.contains(1)).toEqual(true);
+    expect(binarySearchTree.contains(21)).toEqual(false);
+  });
+
+  it('counter should be number of insertions ', function(){
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(7);
+    var counter = 0;
+    binarySearchTree.depthFirstLog(function(){
+      counter++;
+    });
+    expect(counter).toBe(2);
+  });
+
 });
--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,45 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(insvalue){
+  var newTree = {};
+  newTree.parent = null;
+  newTree.value = insvalue;
+  newTree.left = [];
+  newTree.right = [];
+  extend(newTree, treeMethods);
+  return newTree;
+};
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
+
+var each = function(collection, iterator){
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+      iterator(collection[i], i, collection);
+    }
+  } else {
+    for (var j in collection) {
+      iterator(collection[j], j, collection);
+      }
+    }
+  };
+
+var treeMethods = {};
+
+treeMethods.insert = function (val) {
+  var newval = makeBinarySearchTree(val);
+  val < this.value ? this.left.push(newval) : this.right.push(newval);
+};
+
+treeMethods.contains = function(item) {
+  return this.value === item || _.any(_.invoke(this.left, 'contains', item)) || _.any(_.invoke(this.right, 'contains', item));
+};
+
+treeMethods.depthFirstLog = function(callback) {
+  each(this.left, callback);
+  each(this.right, callback);
+  each(this.value, callback);
+  return;
 };
